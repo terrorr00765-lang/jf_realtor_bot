87b60ad67b1adb21331df04b90c1a31c4d1f589d
terrorr00765-lang <terrorr00765@gmail.com>
2025-12-03 00:55:47 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TOKEN = os.getenv("BOT_TOKEN", "8541819226:AAEuKD1vk2VE3PWdYE-28ZlPN2X9Y8CkFz0)
+
+ADMIN_ID = None  # при желании можно указать свой Telegram ID
+BOT_NAME = "Rieltor_JF_bot"
+DATABASE_URL = "sqlite:///database.db"
